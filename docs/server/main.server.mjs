6b4828ba7470b68cbfeaab8d2fd45f3e4f77614e
KIET112345 <vankietclbk@gmail.com>
2025-03-02 00:45:02 +0700

    refactor: remove docs folder and change configuration

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{d as e,e as t,g as o,h as p,i as m}from"./chunk-UM6TDFR3.mjs";import{$b as r}from"./chunk-RAHLL6AW.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
